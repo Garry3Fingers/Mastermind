776c907ba526699f2131d437142b7e76f56640b7
Garry3Fingers <www.vlad78@yandex.ru>
2023-01-09 16:13:27 +0300

    Create methods player_make_guess and player_input

--- main.rb
+++ main.rb
@@ -5,11 +5,38 @@ class GameBoard
     @colors = %w[red blue green yellow orange purple]
   end
 
+  private
+
+  def player_input
+    input = gets.chomp
+    raise InvalidInput, 'Invalid color. Please enter the color from the list' unless @colors.include?(input.downcase)
+  rescue InvalidInput => e
+    puts e
+    retry
+  else
+    input.downcase
+  end
+
+  public
+
   def pc_make_code
     @colors.rotate(rand(1..100))[0, 4]
   end
+
+  def player_make_guess
+    puts "Choose four colors from the 'Red, Blue, Green, Yellow, Orange, Purple' list."
+    puts "Press 'Enter' after each selection."
+    player_guess = []
+    4.times do
+      player_guess << player_input
+    end
+  end
 end
+
+class InvalidInput < StandardError; end
 
 game_board = GameBoard.new
 
 p game_board.pc_make_code
+
+game_board.player_make_guess
