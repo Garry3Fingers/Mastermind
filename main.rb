6d8cfae1c0cef8c45fe45256273ebf81017c8558
Garry3Fingers <www.vlad78@yandex.ru>
2023-01-21 16:37:43 +0300

    Create methods for computer AI

--- main.rb
+++ main.rb
@@ -5,9 +5,11 @@ require 'colorize'
 # It's a test class
 class GameBoard
   attr_reader :colors
+  attr_accessor :set_of_s
 
   def initialize
     @colors = %w[red blue green yellow orange purple]
+    @set_of_s = []
   end
 
   private
@@ -46,11 +48,14 @@ class GameBoard
   def guess_feedback(code, guess_code)
     code.each_with_index.map do |_, i|
       if black_test(code[i], guess_code[i])
-        'O'.colorize(:black)
+        # 'O'.colorize(:black)
+        1
       elsif white_test(code, guess_code, guess_code[i], i)
-        'O'.colorize(:light_white)
+        # 'O'.colorize(:light_white)
+        2
       else
-        'O'.colorize(:red)
+        # 'O'.colorize(:red)
+        3
       end
     end
   end
@@ -58,15 +63,17 @@ class GameBoard
   def white_test(code, guess_code, position, index)
     if guess_code.count(position) == 1 && code.include?(position)
       true
-    elsif guess_code.count(position) > 1 && code.include?(position) && guess_code.index(position) == guess_code[index]
+    elsif guess_code.count(position) > 1 && code.include?(position) && guess_code.index(position) == index\
+      &&  black_test(code[guess_code.rindex(position)], guess_code[guess_code.rindex(position)]) == false
       true
     end
   end
 
   def black_test(code, guess_code)
-    return unless code == guess_code
+    # return unless code == guess_code
 
-    true
+    # true
+    code == guess_code
   end
 
   def show_guess_feedback(code, guess_code)
@@ -100,6 +107,54 @@ within eight turns.
     end
     puts 'Game over! You have no attempts left!' if i.zero?
   end
+
+  def process_feedback(feedback, guess_code)
+    feedback.each_with_index do |number, index|
+      @set_of_s.reject! do |arr|
+        case number
+        when 1
+          arr[index] != guess_code[index]
+        when 2
+          arr.include?(guess_code[index]) == false || arr.count(guess_code[index]) > 1
+        when 3
+          arr.count(guess_code[index]) > 1
+        end
+      end
+    end
+
+    @set_of_s.sample
+  end
+
+  def computer_loop
+    code = computer_make_code
+    i = 8
+    while i.positive?
+      puts "You left #{i} attempts to break the code!"
+
+      guess_code = if i == 8
+                     computer_first_guess(combinations)
+                   else
+                     process_feedback(guess_feedback(code, guess_code), guess_code)
+                   end
+
+      break if compare_arr(code, guess_code)
+
+      p guess_feedback(code, guess_code)
+
+      i -= 1
+    end
+    puts 'Game over! You have no attempts left!' if i.zero?
+  end
+
+  def combinations
+    @colors.repeated_permutation(4) { |permutation| @set_of_s << permutation }
+    @set_of_s
+  end
+
+  def computer_first_guess(combination_arr)
+    index = combination_arr.index { |arr| arr == %w[red red blue blue] }
+    combination_arr[index]
+  end
 end
 
 class InvalidInput < StandardError; end
@@ -107,6 +162,5 @@ class InvalidInput < StandardError; end
 game_board = GameBoard.new
 
 # p String.colors
-puts game_board.show_start_message
 
-game_board.game_loop
+game_board.computer_loop
